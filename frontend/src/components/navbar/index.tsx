9e03cf4d0a4fce407a815f2d2d1ee830b0b88cf9
Igor Mendes <igor07082001@gmail.com>
2022-05-14 17:03:54 -0300

    navbar

---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GitHubIcon } from "assets/img/github.svg";
+import './styles.css'
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/IgorSMendes2001">
+            <div className="dsmovie-contact-container">
+              <GitHubIcon />
+              <p className="dsmovie-contact-link">/IgorSMendes2001</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+export default Navbar;
