eadb8de7f80bac1a44d74a76420800c1c78f5182
Igor Mendes <igor07082001@gmail.com>
2022-05-15 00:22:19 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/dsmovie/backend/entities/Score.java
@@ -0,0 +1,34 @@
+package com.dsmovie.backend.entities;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_score")
+public class Score {
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+    public Score(){
+
+    }
+    public ScorePK getId() {
+        return id;
+    }
+    public void setMovie(Movie movie){
+        id.setMovie(movie);
+    }
+    public void setUser(User user){
+        id.setUser(user);
+    }
+    public void setId(ScorePK id) {
+        this.id = id;
+    }
+    public Double getValue() {
+        return value;
+    }
+    public void setValue(Double value) {
+        this.value = value;
+    }
+    
+}
