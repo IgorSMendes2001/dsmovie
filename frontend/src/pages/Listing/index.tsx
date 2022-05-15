83a4e72ede6ae70f7c13e7c9997549c55363d8f3
Igor Mendes <igor07082001@gmail.com>
2022-05-15 19:15:39 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -25,11 +25,13 @@ function Listing(){
                setPage(data);
           });
      },[pageNumber]);
-   
+     const handlePageChange=(newPageNumber:number)=>{
+          setPageNumber(newPageNumber);
+     }
 
     return (
         <>
-        <Pagination/>
+        <Pagination page={page} onChange={handlePageChange}/>
         <div className="container">
         <div className="row">
           {page.content.map(movie=>(
