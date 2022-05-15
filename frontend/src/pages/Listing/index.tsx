43ee536215cb16b26d561704c09ace93912109f3
Igor Mendes <igor07082001@gmail.com>
2022-05-15 19:04:33 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -19,7 +19,7 @@ function Listing(){
           empty: true
      });
      useEffect(()=>{
-          axios.get(`${BASE_URL}/movies?size=12&&page=${pageNumber}&sort=title`)
+          axios.get(`${BASE_URL}/movies?size=12&&page=${pageNumber}&sort=id`)
           .then(response=>{
                const data=response.data as MoviePage;
                setPage(data);
